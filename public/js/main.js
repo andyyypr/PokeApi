99fade784edbb935198e4dbe1c22b5366fd45840
andyyypr <andyismaelpr05@gmail.com>
2025-10-06 23:49:17 -0400

    nuevas implementaciones

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,29 @@
-const PokeApiUrl = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+let limit = 40;
+let offset = 0;
+
+function getPokeApiUrl() {
+  return `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+}
 
 async function dataPkmns(pkmnUrl) {
   const response = await fetch(pkmnUrl);
   return await response.json();
 }
-
+//Funcion para convertir la primera letra en Mayuscula
+function capitalizar(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+//Funcion para formatear numeros
+function formatearNumero(num) {
+  if (num < 10) return `00${num}`;
+  if (num < 100) return `0${num}`;
+  return `${num}`;
+}
+// Función para limpiar el contenedor
+function clearContainer() {
+  const container = document.querySelector(".card__container");
+  container.innerHTML = "";
+}
 // Función para crear la card
 async function createHtmlCard(data) {
   const container = document.querySelector(".card__container");
@@ -30,18 +49,32 @@ async function createHtmlCard(data) {
       const tipos = info.types.map((t) => t.type.name).join(" - ");
 
       cardInner.innerHTML = `
-          
-          <div class="card__face front">
-            <p class="num_pkm">${info.id}</p>
-            <img class="img_pkm" src=" ${info.sprites.front_default}" alt="${info.name}" />
-            <h2 class="name_pkm">${info.name}</h2>
-            <div class="data__container">
-              <h3 id="weight_pkm">Weight: ${info.weight} kg</h3>
-              <h3 id="height_pkm">Height: ${info.height}</h3>
-            </div>
-            <h3 id="type_pkmn">Type: ${tipos}</h3>
+        <div class="card__face front">
+          <div class="up_container">
+            <p class="num_pkm">#${formatearNumero(info.id)}</p>
+            <div class="color_type"></div>
           </div>
-
+          
+          <img class="img_pkm" src="${info.sprites.front_default}" alt="${
+        info.name
+      }" />
+          <h2 class="name_pkm">${capitalizar(info.name)}</h2>
+          <div class="data__container">
+            <div class="info__item">
+              <label>Weight</label>
+              <p>${info.weight} kg</p>
+            </div>
+            <div class="info__item">
+              <label>Height</label>
+              <p>${info.height}</p>
+            </div>
+          </div>
+          <div class="type__container">
+            <label>Type</label>
+            <p>${tipos}</p>
+          </div>
+        </div>
+          
         
           <div class="card__face back-1">
             <h2>Data</h2>
@@ -60,11 +93,11 @@ async function createHtmlCard(data) {
       const listMoves = document.createElement("div");
       listMoves.classList.add("movements__list");
 
-      for (const movements of info.moves) {
+      const limitedMoves = info.moves.slice(0, 10); // Mostrar solo primeros 10 movimientos
+      for (const movements of limitedMoves) {
         const movem = document.createElement("h3");
         movem.classList.add("movements__list-item");
         movem.textContent = movements.move.name;
-
         listMoves.appendChild(movem);
       }
       cardFace2.appendChild(listMoves);
@@ -73,14 +106,14 @@ async function createHtmlCard(data) {
       card.appendChild(cardInner);
       container.appendChild(card);
 
+      listMoves.innerHTML = "";
+
       //evento para girar las cards
       let currentFace = 0;
       card.addEventListener("click", () => {
         currentFace = (currentFace + 1) % 3;
         cardInner.style.transform = `rotateY(${currentFace * 120}deg)`;
       });
-
-      console.log("Card created");
     } catch (error) {
       console.error(`Error cargando ${pokemon.name}:`, error);
     }
@@ -91,7 +124,7 @@ async function createHtmlCard(data) {
 // Función principal para obtener los datos y crear las cards
 async function getInfoAllPkms() {
   try {
-    const response = await fetch(PokeApiUrl);
+    const response = await fetch(getPokeApiUrl());
     const data = await response.json();
     await createHtmlCard(data);
   } catch (error) {
@@ -101,5 +134,22 @@ async function getInfoAllPkms() {
 
 // Ejecutar al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
+  const pageLeft_Btm = document.querySelector(".page-left");
+  const pageRight_Btm = document.querySelector(".page-right");
+
+  pageRight_Btm.addEventListener("click", function () {
+    offset += limit;
+    clearContainer();
+    getInfoAllPkms();
+  });
+
+  pageLeft_Btm.addEventListener("click", function () {
+    if (offset > 0) {
+      offset -= limit;
+      clearContainer();
+      getInfoAllPkms();
+    }
+  });
+
   getInfoAllPkms();
 });
